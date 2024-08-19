07efeacaa464508d283eb741aa53bf5bfdf2a2e4
ackrilll <112237701+ackrilll@users.noreply.github.com>
2024-08-19 15:45:18 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 10.60 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static long solution(long n) {
+        long answer = -1;
+        for(long i = 1; n >= i*i; i++ ){
+            if (n ==i*i){
+                answer = (i+1)*(i+1);
+                break;}
+        }
+        return answer;
+    }
+}
