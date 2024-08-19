c6e49ffdd4a4ce7f0db68f97e10a14cd7a14c4a3
ackrilll <112237701+ackrilll@users.noreply.github.com>
2024-08-19 15:46:15 +0900

    [level 1] Title: 평균 구하기, Time: 0.08 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static double solution(int[] arr) {
+        double answer = 0;
+        int length = arr.length;
+        int sum = 0;
+        for (int i = 0; i < length ;i++){
+            sum += arr[i];
+        }
+        answer = (double) sum / length;
+        return answer;
+    }
+}
